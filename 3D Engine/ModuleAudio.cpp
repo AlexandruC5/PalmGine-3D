f706b16920290af2645ef678e9a40cf8180937d3
manelmourelo <manelmourelo.1@gmail.com>
2018-09-23 16:30:21 +0200

    Deleting p2containers

--- 3D Engine/ModuleAudio.cpp
+++ 3D Engine/ModuleAudio.cpp
@@ -53,12 +53,11 @@ bool ModuleAudio::CleanUp()
 	{
 		Mix_FreeMusic(music);
 	}
+	std::list<Mix_Chunk*>::iterator item;
 
-	p2List_item<Mix_Chunk*>* item;
-
-	for(item = fx.getFirst(); item != NULL; item = item->next)
+	for(item = fx.begin(); item != fx.end(); item++)
 	{
-		Mix_FreeChunk(item->data);
+		Mix_FreeChunk(item._Ptr->_Myval);
 	}
 
 	fx.clear();
@@ -132,8 +131,8 @@ unsigned int ModuleAudio::LoadFx(const char* path)
 	}
 	else
 	{
-		fx.add(chunk);
-		ret = fx.count();
+		fx.push_back(chunk);
+		ret = fx.size();
 	}
 
 	return ret;
@@ -146,7 +145,7 @@ bool ModuleAudio::PlayFx(unsigned int id, int repeat)
 
 	Mix_Chunk* chunk = NULL;
 	
-	if(fx.at(id-1, chunk) == true)
+	if(id > 0 && id <= fx.size())
 	{
 		Mix_PlayChannel(-1, chunk, repeat);
 		ret = true;
