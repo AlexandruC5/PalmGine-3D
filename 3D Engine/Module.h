489494c87e4c4448663d173ef2c5dc55447380c8
manelmourelo <manelmourelo.1@gmail.com>
2018-11-02 12:23:27 +0100

    Delete PhysBody3D

--- 3D Engine/Module.h
+++ 3D Engine/Module.h
@@ -2,7 +2,6 @@
 #define MODULE_H
 
 class Application;
-struct PhysBody3D;
 
 class Application;
 
@@ -48,9 +47,6 @@ public:
 	{ 
 		return true; 
 	}
-
-	virtual void OnCollision(PhysBody3D* body1, PhysBody3D* body2)
-	{}
 };
 
 #endif // !MODULE_H
