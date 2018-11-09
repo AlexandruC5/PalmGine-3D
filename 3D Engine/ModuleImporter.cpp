14a94839827ba5f0ae831d11be9979d45555b5ee
Fran <fran152job@gmail.com>
2018-11-09 20:41:53 +0100

    Fixed bug with meshes

--- 3D Engine/ModuleImporter.cpp
+++ 3D Engine/ModuleImporter.cpp
@@ -56,193 +56,15 @@ bool ModuleImporter::LoadFBX(const char* path)
 	
 	const aiScene* scene = aiImportFile(path, aiProcessPreset_TargetRealtime_MaxQuality);
 
-	std::string binary_path = ImportFBX(scene, path, "temporal");
+	std::string binary_path = ImportFBX(scene, path, GetFileNameFromPath(path).c_str());
 
 	char* data = LoadData(binary_path.c_str());
 	char* cursor = data;
 
 	LoadRecursiveHierarchy(&cursor, App->scene_intro->root_gameObjects);
 
-	/*if (scene != nullptr && scene->HasMeshes())
-	{
-		aiNode* rootNode = scene->mRootNode;
-		for (int i = 0; i < rootNode->mNumChildren; ++i)
-		{
-			GameObject* tmp = LoadModel(scene, rootNode->mChildren[i], path);
-			App->scene_intro->root_gameObjects->AddChild(tmp);
-			App->scene_intro->SetSelectedGameObject(tmp);
-		}
-		
-		// ---- Release resources ----
-		aiReleaseImport(scene);
-		LOG("---- FBX LOADED WITH SUCCESS ----");
-		return true;
-	}
-	else
-		LOG("---- ERROR, COULDN'T LOAD FBX ----");*/
 	return false;
 }
-
-/*GameObject* ModuleImporter::LoadModel(const aiScene* scene, aiNode* node, const char* path)
-{
-	GameObject* temp_go = new GameObject(nullptr);
-	temp_go->SetName((char*)node->mName.C_Str());
-	
-	if (node->mNumMeshes <= 0)
-	{
-		LOG("Unable to load the mesh with path: %s. The number of meshes is below or equal to 0.", path);
-	}
-	else
-	{
-		// Setting GameObject components
-		CompMesh* mesh_comp = new CompMesh(temp_go, COMP_TYPE::C_MESH);
-		Mesh* temp_mesh = new Mesh();
-		CompMaterial* mat_comp = new CompMaterial(temp_go, COMP_TYPE::C_MATERIAL);
-
-		LOG("Loading mesh from path %s", path);
-		for (int i = 0; i < node->mNumMeshes; i++)
-		{
-			aiMesh* new_mesh = scene->mMeshes[node->mMeshes[i]];
-			
-			temp_mesh->num_vertices = new_mesh->mNumVertices;
-			temp_mesh->vertices = new float[temp_mesh->num_vertices * 3];
-			memcpy(temp_mesh->vertices, new_mesh->mVertices, sizeof(float)*temp_mesh->num_vertices * 3);
-
-			// ---- Generate buffers ----
-			glGenBuffers(1, (GLuint*)&(temp_mesh->id_vertices));
-			glBindBuffer(GL_ARRAY_BUFFER, temp_mesh->id_vertices);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * temp_mesh->num_vertices, temp_mesh->vertices, GL_STATIC_DRAW);
-			
-			// ---- Mesh ----
-			if (new_mesh->HasFaces())
-			{
-				temp_mesh->num_indices = new_mesh->mNumFaces * 3;
-				temp_mesh->indices = new uint[temp_mesh->num_indices];
-				for (uint j = 0; j < new_mesh->mNumFaces; j++)
-				{
-					if (new_mesh->mFaces[j].mNumIndices == 3)
-					{
-						memcpy(&temp_mesh->indices[j * 3], new_mesh->mFaces[j].mIndices, 3 * sizeof(uint));
-					}
-				}
-
-				glGenBuffers(1, (GLuint*)&(temp_mesh->id_indices));
-				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, temp_mesh->id_indices);
-				glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * temp_mesh->num_indices, temp_mesh->indices, GL_STATIC_DRAW);
-			}
-			else
-			{
-				LOG("Mesh with %i faces can not be loaded.", new_mesh->mNumFaces);
-			}
-
-			// ---- Texture ----
-			if (new_mesh->HasTextureCoords(temp_mesh->id_uvs))
-			{
-				// ---- UVs ----
-				temp_mesh->num_uvs = new_mesh->mNumVertices;
-				temp_mesh->uvs = new float[temp_mesh->num_uvs * 2];
-
-				for (int i = 0; i < new_mesh->mNumVertices; ++i)
-				{
-					memcpy(&temp_mesh->uvs[i * 2], &new_mesh->mTextureCoords[0][i].x, sizeof(float));
-					memcpy(&temp_mesh->uvs[(i * 2) + 1], &new_mesh->mTextureCoords[0][i].y, sizeof(float));
-				}
-
-				glGenBuffers(1, (GLuint*)&(temp_mesh->id_uvs));
-				glBindBuffer(GL_ARRAY_BUFFER, temp_mesh->id_uvs);
-				glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 2 * temp_mesh->num_uvs, temp_mesh->uvs, GL_STATIC_DRAW);
-			}
-			else
-			{
-				LOG("Texture coords couldn´t be found for the specified mesh.");
-			}
-
-			// ---- Texture ID ----
-			aiMaterial* material = scene->mMaterials[new_mesh->mMaterialIndex];
-			if (material)
-			{
-				aiString path;
-				material->GetTexture(aiTextureType_DIFFUSE, 0, &path);
-
-				if (path.length > 0)
-				{
-					// ---- Find the texture on textures folder ----
-					std::string texture_folder = "Textures/";
-					std::string final_path = path.data;
-					final_path.erase(0, final_path.find_last_of("\\") + 1);
-					texture_folder += final_path;
-
-					mat_comp->SetID(CreateTextureID(texture_folder.c_str()), texture_folder, ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT));
-					LOG("Texture with path %s has been loaded.", texture_folder.c_str());
-					final_path.clear();
-					texture_folder.clear();
-				}
-			}
-
-			// ---- Normals ----
-			if (new_mesh->HasNormals())
-			{
-				temp_mesh->num_normals = new_mesh->mNumVertices;
-				temp_mesh->normals = new float[temp_mesh->num_normals * 3];
-				memcpy(temp_mesh->normals, new_mesh->mNormals, sizeof(float) * temp_mesh->num_normals * 3);
-
-				glGenBuffers(1, (GLuint*)&(temp_mesh->id_normals));
-				glBindBuffer(GL_ARRAY_BUFFER, temp_mesh->id_normals);
-				glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * temp_mesh->num_normals, temp_mesh->normals, GL_STATIC_DRAW);
-			}
-			else
-			{
-				LOG("Mesh has no normals.");
-			}
-			 
-			// ---- Push the mesh ----
-			mesh_comp->SetMesh(temp_mesh);
-			LOG("Loaded mesh with %i vertices.", temp_mesh->num_vertices);
-			LOG("Loaded mesh with %i indices.", temp_mesh->num_indices);
-			LOG("Loaded mesh with %i triangles.", temp_mesh->num_vertices / 3);
-			LOG("Loaded mesh with %i normals.", temp_mesh->num_normals);
-			LOG("Loaded mesh with %i uvs.", temp_mesh->num_uvs);
-		}
-		temp_go->AddComponent(mesh_comp);
-		temp_go->AddComponent(mat_comp);
-	}
-
-	for (int i = 0; i < node->mNumChildren; i++)
-	{
-		temp_go->AddChild(LoadModel(scene, node->mChildren[i], path));
-	}
-
-	//math::float4x4 matrix;
-	//matrix.Set(node->mTransformation.a1, node->mTransformation.a2, node->mTransformation.a3, node->mTransformation.a4,
-	//	node->mTransformation.b1, node->mTransformation.b2, node->mTransformation.b3, node->mTransformation.b4,
-	//	node->mTransformation.c1, node->mTransformation.c2, node->mTransformation.c3, node->mTransformation.c4,
-	//	node->mTransformation.d1, node->mTransformation.d2, node->mTransformation.d3, node->mTransformation.d4);
-
-	//temp_go->GetCompTransform()->SetTransformation(matrix);
-
-	aiVector3D translation;
-	aiVector3D scaling;
-	aiQuaternion rotation;
-	node->mTransformation.Decompose(scaling, rotation, translation);
-	aiMatrix3x3 rotMat = rotation.GetMatrix();
-	aiVector3D rotationEuler = rotMat.GetEuler();
-
-	float3 pos;
-	pos.Set(translation.x, translation.y, translation.z);
-	float3 rot;
-	rot.Set(rotationEuler.x, rotationEuler.y, rotationEuler.z);
-	float3 scale;
-	scale.Set(scaling.x, scaling.y, scaling.z);
-
-	temp_go->GetCompTransform()->SetRotation(rot);
-	temp_go->GetCompTransform()->SetScale(scale);
-	temp_go->GetCompTransform()->SetPosition(pos);
-
-	LOG("GameObject position: (%f, %f, %f)", temp_go->GetCompTransform()->GetPosition().x, temp_go->GetCompTransform()->GetPosition().y, temp_go->GetCompTransform()->GetPosition().z);
-	LOG("GameObject rotation: (%f, %f, %f)", temp_go->GetCompTransform()->GetRotation().x, temp_go->GetCompTransform()->GetRotation().y, temp_go->GetCompTransform()->GetRotation().z);
-	LOG("GameObject scale: (%f, %f, %f)", temp_go->GetCompTransform()->GetScale().x, temp_go->GetCompTransform()->GetScale().y, temp_go->GetCompTransform()->GetScale().z);
-	return temp_go;
-}*/
 
 std::string ModuleImporter::ImportFBX(const aiScene * scene, const char * path, const char * name)
 {
@@ -308,7 +130,7 @@ void ModuleImporter::WriteBinaryRecursive(aiNode * node, char ** cursor, const c
 				if (mesh->mFaces[i].mNumIndices != 3)
 				{
 					LOG("WARNING, geometry face with != 3 faces");
-					range[i] = 0;
+					range[0] = 0;
 				}
 			}
 			aiString tmp_path;
@@ -613,8 +435,9 @@ GameObject* ModuleImporter::ReadBinaryHierarchy(char** cursor, uint* num_childs,
 		// *-- CREATE GAME OBJECT --*
 		go = new GameObject(parent);
 		go->SetName(name);
-		LoadDDS(texture_name, go);
-		ReadBinaryMesh(path_name, go);
+		//LoadDDS(texture_name, go);
+		if(range[0] != 0)
+			ReadBinaryMesh(path_name, go);
 
 		iterator++;
 	} while (iterator < num_meshes);
