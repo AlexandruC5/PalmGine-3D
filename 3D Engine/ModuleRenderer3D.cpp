b0819f21e58a68a91732002c71f81da65b2cd047
Fran <fran152job@gmail.com>
2018-11-10 14:39:46 +0100

    Scene background color change

--- 3D Engine/ModuleRenderer3D.cpp
+++ 3D Engine/ModuleRenderer3D.cpp
@@ -73,7 +73,7 @@ bool ModuleRenderer3D::Init()
 			glClearDepth(1.0f);
 
 			//Initialize clear color
-			glClearColor(0.f, 0.f, 0.f, 1.f);
+			glClearColor(0.220, 0.220, 0.220, 1.f);
 
 			//Check for error
 			error = glGetError();
