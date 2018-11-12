a7a8626db6061caa22b9acccc30027fb7033a00b
manelmourelo <manelmourelo.1@gmail.com>
2018-11-12 09:28:44 +0100

    Button to delete GameObjectsWorkInProgress

--- 3D Engine/PanelInspector.cpp
+++ 3D Engine/PanelInspector.cpp
@@ -31,125 +31,138 @@ void PanelInspector::Draw()
 		if(ImGui::InputText("Name", name, 50, ImGuiInputTextFlags_AutoSelectAll |ImGuiInputTextFlags_EnterReturnsTrue)) {
 			selected_go->SetName(name);
 		}
+		
+		//DeleteGameObject
+		//TODO Solve crash when delete is clicked in this commented code
+		//if (selected_go != App->scene_intro->camera) {
+		//	ImGui::SameLine();
+		//	if (ImGui::Button("Delete")) {
+		//		App->scene_intro->quadtree.Remove(selected_go);
+		//		delete selected_go;
+		//		App->scene_intro->selected_gameObject = nullptr;
+		//		selected_go = nullptr;
+		//	}
+		//}
 
 		// Components
-		CompTransform* transform = selected_go->GetCompTransform();
-		CompMesh* mesh = selected_go->GetCompMesh();
-		CompMaterial* texture = selected_go->GetCompMaterial();
-		CompCamera* camera = selected_go->GetCompCamera();
+		if (selected_go != nullptr) {
+			CompTransform* transform = selected_go->GetCompTransform();
+			CompMesh* mesh = selected_go->GetCompMesh();
+			CompMaterial* texture = selected_go->GetCompMaterial();
+			CompCamera* camera = selected_go->GetCompCamera();
 
-		if (mesh != nullptr) {
-			//SetStatic
-			bool isStatic = selected_go->IsStatic();
-			if (ImGui::Checkbox("Static", &isStatic)) {
-				selected_go->SetStatic(isStatic);
-				if (isStatic == true) {
-					App->scene_intro->quadtree.Insert(selected_go);
-				}
-				else {
-					App->scene_intro->quadtree.Remove(selected_go);
-				}
-			}
-		}
-
-		if (ImGui::CollapsingHeader("Transform"), ImGuiTreeNodeFlags_DefaultOpen) 
-		{
-			if (transform != nullptr)
-			{
-				//Enable guizmos
-				EnableGuizmos(selected_go);
-
-				ImGui::Text("Showing read only information about the mesh transform");
-				ImGui::Separator();
-				//Position
-				ImGui::Text("Position:");
-				if (selected_go->IsStatic() == false) {
-					float3 pos = transform->GetPosition();
-					if (ImGui::DragFloat3("Position", (float*)&pos, 0.5f)) {
-						transform->SetPosition(pos);
+			if (mesh != nullptr) {
+				//SetStatic
+				bool isStatic = selected_go->IsStatic();
+				if (ImGui::Checkbox("Static", &isStatic)) {
+					selected_go->SetStatic(isStatic);
+					if (isStatic == true) {
+						App->scene_intro->quadtree.Insert(selected_go);
+					}
+					else {
+						App->scene_intro->quadtree.Remove(selected_go);
 					}
 				}
-				else {
-					ImGui::Text("[%f]    [%f]    [%f]", transform->GetPosition().x, transform->GetPosition().y, transform->GetPosition().z);
-				}
-				//Rotation
-				ImGui::Text("Rotation:");
-				if (selected_go->IsStatic() == false) {
-					float3 rot = transform->GetRotation();
-					if (ImGui::DragFloat3("Rotation", (float*)&rot, -pi, pi)) {
-						transform->SetRotation(rot);
+			}
+
+			if (ImGui::CollapsingHeader("Transform"), ImGuiTreeNodeFlags_DefaultOpen)
+			{
+				if (transform != nullptr)
+				{
+					//Enable guizmos
+					EnableGuizmos(selected_go);
+
+					ImGui::Text("Showing read only information about the mesh transform");
+					ImGui::Separator();
+					//Position
+					ImGui::Text("Position:");
+					if (selected_go->IsStatic() == false) {
+						float3 pos = transform->GetPosition();
+						if (ImGui::DragFloat3("Position", (float*)&pos, 0.5f)) {
+							transform->SetPosition(pos);
+						}
+					}
+					else {
+						ImGui::Text("[%f]    [%f]    [%f]", transform->GetPosition().x, transform->GetPosition().y, transform->GetPosition().z);
+					}
+					//Rotation
+					ImGui::Text("Rotation:");
+					if (selected_go->IsStatic() == false) {
+						float3 rot = transform->GetRotation();
+						if (ImGui::DragFloat3("Rotation", (float*)&rot, -pi, pi)) {
+							transform->SetRotation(rot);
+						}
+					}
+					else {
+						ImGui::Text("[%f]    [%f]    [%f]", transform->GetRotation().x, transform->GetRotation().y, transform->GetRotation().z);
+					}
+					//Scale
+					ImGui::Text("Scale:");
+					if (selected_go->IsStatic() == false) {
+						float3 scale = transform->GetScale();
+						if (ImGui::DragFloat3("Scale", (float*)&scale, 0.05f)) {
+							transform->SetScale(scale);
+						}
+					}
+					else {
+						ImGui::Text("[%f]    [%f]    [%f]", transform->GetScale().x, transform->GetScale().y, transform->GetScale().z);
 					}
 				}
-				else {
-					ImGui::Text("[%f]    [%f]    [%f]", transform->GetRotation().x, transform->GetRotation().y, transform->GetRotation().z);
-				}
-				//Scale
-				ImGui::Text("Scale:");
-				if (selected_go->IsStatic() == false) {
-					float3 scale = transform->GetScale();
-					if (ImGui::DragFloat3("Scale", (float*)&scale, 0.05f)) {
-						transform->SetScale(scale);
+				else
+					LOG("ERROR: COMPONENT TRANSFORM IS NULLPTR on GameObject with name %s", selected_go->GetName().c_str());
+			}
+
+			if (mesh != nullptr)
+			{
+				if (ImGui::CollapsingHeader("Renderer Options"), ImGuiTreeNodeFlags_DefaultOpen)
+				{
+					ImGui::Checkbox("Wireframe mode", &mesh->wireframe);
+					if (texture != nullptr) {
+						ImGui::SliderFloat("Alpha tolerance", &mesh->tex_alpha, 0, 1, NULL);
 					}
 				}
-				else {
-					ImGui::Text("[%f]    [%f]    [%f]", transform->GetScale().x, transform->GetScale().y, transform->GetScale().z);
+				if (ImGui::CollapsingHeader("Mesh Information"), ImGuiTreeNodeFlags_DefaultOpen)
+				{
+					ImGui::Text("Showing read only information about the mesh");
+					ImGui::Separator();
+					//Mesh Triangles
+					ImGui::Text("Mesh triangles: %i", mesh->GetNumVertices() / 3);
+					//Mesh Vertices
+					ImGui::Text("Mesh vertices: %i", mesh->GetNumVertices());
+					//Mesh Indices
+					ImGui::Text("Mesh indices: %i", mesh->GetNumIndices());
+					//Mesh Normals
+					ImGui::Text("Mesh normals: %f", mesh->GetNumNormals());
+					//Mesh UVS
+					ImGui::Text("Mesh uvs: %f", mesh->GetNumUvs());
 				}
 			}
-			else
-				LOG("ERROR: COMPONENT TRANSFORM IS NULLPTR on GameObject with name %s", selected_go->GetName().c_str());
-		}
 
-		if (mesh != nullptr)
-		{
-			if (ImGui::CollapsingHeader("Renderer Options"), ImGuiTreeNodeFlags_DefaultOpen)
-			{
-				ImGui::Checkbox("Wireframe mode", &mesh->wireframe);
-				if (texture != nullptr) {
-					ImGui::SliderFloat("Alpha tolerance", &mesh->tex_alpha, 0, 1, NULL);
+			if (camera != nullptr) {
+				if (ImGui::CollapsingHeader("Camera"), ImGuiTreeNodeFlags_DefaultOpen)
+				{
+					ImGui::Checkbox("Active", &camera->active);
+					ImGui::Checkbox("Frustum Culling", &camera->frustum_culling);
+					float near_plane_dis = camera->GetNearPlaneDistance();
+					if (ImGui::DragFloat("Near Plane", &near_plane_dis, 0.1f, 0.1f, 1000.0f)) {
+						camera->SetNearPlaneDistance(near_plane_dis);
+					}
+					float far_plane_dis = camera->GetFarPlaneDistance();
+					if (ImGui::DragFloat("Far Plane", &far_plane_dis, 0.1f, 25.f, 1000.0f)) {
+						camera->SetFarPlaneDistance(far_plane_dis);
+					}
+					float fov = camera->GetFOV();
+					if (ImGui::SliderFloat("Field of View", &fov, 1.0f, 179.0f)) {
+						camera->SetFOV(fov);
+					}
+					float aspect_ratio = camera->GetApectRatio();
+					if (ImGui::DragFloat("Aspect Ratio", &aspect_ratio, 0.1f, 0.1f, 1000.0f)) {
+						camera->SetAspectRatio(aspect_ratio);
+					}
 				}
 			}
-			if (ImGui::CollapsingHeader("Mesh Information"), ImGuiTreeNodeFlags_DefaultOpen)
-			{
-				ImGui::Text("Showing read only information about the mesh");
-				ImGui::Separator();
-				//Mesh Triangles
-				ImGui::Text("Mesh triangles: %i", mesh->GetNumVertices() / 3);
-				//Mesh Vertices
-				ImGui::Text("Mesh vertices: %i", mesh->GetNumVertices());
-				//Mesh Indices
-				ImGui::Text("Mesh indices: %i", mesh->GetNumIndices());
-				//Mesh Normals
-				ImGui::Text("Mesh normals: %f", mesh->GetNumNormals());
-				//Mesh UVS
-				ImGui::Text("Mesh uvs: %f", mesh->GetNumUvs());
-			}
-		}
 
-		if (camera != nullptr) {
-			if (ImGui::CollapsingHeader("Camera"), ImGuiTreeNodeFlags_DefaultOpen)
-			{
-				ImGui::Checkbox("Active", &camera->active);
-				ImGui::Checkbox("Frustum Culling", &camera->frustum_culling);
-				float near_plane_dis = camera->GetNearPlaneDistance();
-				if (ImGui::DragFloat("Near Plane", &near_plane_dis, 0.1f, 0.1f, 1000.0f)) {
-					camera->SetNearPlaneDistance(near_plane_dis);
-				}
-				float far_plane_dis = camera->GetFarPlaneDistance();
-				if (ImGui::DragFloat("Far Plane", &far_plane_dis, 0.1f, 25.f, 1000.0f)) {
-					camera->SetFarPlaneDistance(far_plane_dis);
-				}
-				float fov = camera->GetFOV();
-				if (ImGui::SliderFloat("Field of View", &fov, 1.0f, 179.0f)) {
-					camera->SetFOV(fov);
-				}
-				float aspect_ratio = camera->GetApectRatio();
-				if (ImGui::DragFloat("Aspect Ratio", &aspect_ratio, 0.1f, 0.1f, 1000.0f)) {
-					camera->SetAspectRatio(aspect_ratio);
-				}
-			}
-		}
-	
-	
+
 			// TODO Add mesh info
 			/*{
 			ImGui::Text("Showing read only information about the mesh");
@@ -166,14 +179,15 @@ void PanelInspector::Draw()
 			ImGui::Text("Mesh uvs: %f", App->fbx->GetUvsQuanity());
 		}*/
 
-		if (texture != nullptr) {
-			if (ImGui::CollapsingHeader("Material"), ImGuiTreeNodeFlags_DefaultOpen) {
-				ImGui::Text("Showing read only information about the mesh material");
-				ImGui::Separator();
-				ImGui::Text("Texture path: %s", texture->GetTexturePath().c_str());
-				ImGui::Text("Texture WIDTH: %i", texture->GetTextureWidth());
-				ImGui::Text("Texture HEIGHT: %i", texture->GetTextureHeight());
-				ImGui::Image((ImTextureID)texture->GetTextureId(), ImVec2(200, 200));
+			if (texture != nullptr) {
+				if (ImGui::CollapsingHeader("Material"), ImGuiTreeNodeFlags_DefaultOpen) {
+					ImGui::Text("Showing read only information about the mesh material");
+					ImGui::Separator();
+					ImGui::Text("Texture path: %s", texture->GetTexturePath().c_str());
+					ImGui::Text("Texture WIDTH: %i", texture->GetTextureWidth());
+					ImGui::Text("Texture HEIGHT: %i", texture->GetTextureHeight());
+					ImGui::Image((ImTextureID)texture->GetTextureId(), ImVec2(200, 200));
+				}
 			}
 		}
 	}
