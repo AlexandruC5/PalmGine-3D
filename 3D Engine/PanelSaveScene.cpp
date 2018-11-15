4b42a56b23e4b61ae2ec936e8409f39dd4d9be32
Fran <fran152job@gmail.com>
2018-11-16 00:17:56 +0100

    Save button on Save Scene window

--- 3D Engine/PanelSaveScene.cpp
+++ 3D Engine/PanelSaveScene.cpp
@@ -2,10 +2,15 @@
 #include "Application.h"
 
 PanelSaveScene::PanelSaveScene() : Panel("Save Scene")
-{}
+{
+	name = new char[64];
+	strcpy(name, "scene_name");
+}
 
 PanelSaveScene::~PanelSaveScene()
-{}
+{
+	RELEASE_ARRAY(name);
+}
 
 void PanelSaveScene::Draw()
 {
@@ -13,8 +18,9 @@ void PanelSaveScene::Draw()
 	ImGui::SetWindowPos({ (float)App->window->height/3, ((float)App->window->height - ((225 * App->window->height) / 1024))/2 });
 	ImGui::SetWindowSize(ImVec2(App->window->width/4, (225 * App->window->height) / 1024), ImGuiCond_Always);
 	ImGui::Text("Introduce the name of the scene:");
-	char name[50] = "scene_name";
-	if (ImGui::InputText(".binaryscene", name, 50, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue))
+	ImGui::InputText(".binaryscene", name, 64, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue);
+	
+	if (ImGui::Button("save"))
 	{
 		App->scene_intro->SerializeScene(name);
 		active = false;
