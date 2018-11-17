7c6daad6bb1e7fbab3fffc863797066ff4e04326
Fran <fran152job@gmail.com>
2018-11-17 18:37:29 +0100

    Forget mesh deletes on ResourceMesh

--- 3D Engine/ResourceMesh.cpp
+++ 3D Engine/ResourceMesh.cpp
@@ -1,4 +1,6 @@
 #include "ResourceMesh.h"
+#include "CompMesh.h"
+#include "Globals.h"
 
 ResourceMesh::ResourceMesh(uint uuid)
 {}
@@ -10,5 +12,9 @@ ResourceMesh::ResourceMesh()
 
 ResourceMesh::~ResourceMesh()
 {
+	RELEASE_ARRAY(mesh->indices);
+	RELEASE_ARRAY(mesh->vertices);
+	RELEASE_ARRAY(mesh->normals);
+	RELEASE_ARRAY(mesh->uvs);
 	RELEASE(mesh);
 }
