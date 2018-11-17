dcd234c852193cba95353c2044be8f340f3d9157
Fran <fran152job@gmail.com>
2018-11-17 14:10:47 +0100

    Textures applies correctly in Assets panel

--- 3D Engine/PanelAssets.cpp
+++ 3D Engine/PanelAssets.cpp
@@ -3,8 +3,6 @@
 #include "Globals.h"
 #include "Devil/include/il.h"
 
-#define IL_IMAGE_WIDTH           0x0DE4
-
 PanelAssets::PanelAssets() : Panel("Save Scene")
 {}
 
@@ -28,7 +26,27 @@ void PanelAssets::Draw()
 	{
 		for (uint i = 0; i < textures.size(); i++)
 		{
-			ImGui::Text(textures[i].c_str());
+			if (ImGui::MenuItem(textures[i].c_str()))
+			{
+				if (App->scene_intro->selected_gameObject != nullptr) 
+				{
+					std::string tmp = DDS_IMAGES_PATH;
+					tmp += textures[i];
+					tmp += ".dds";
+
+					uint texture_uuid = App->resource_manager->SearchResource(tmp.c_str());
+
+					if (texture_uuid == 0)
+						App->importer->LoadDDS(tmp.c_str(), App->scene_intro->selected_gameObject);
+					else
+					{
+						CompMaterial* mat_comp = App->scene_intro->selected_gameObject->GetCompMaterial();
+						mat_comp->rtexture = (ResourceTexture*)App->resource_manager->resources[texture_uuid];
+						mat_comp->SetID(mat_comp->rtexture->texture->id, mat_comp->rtexture->exported_path, ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT));
+						mat_comp->rtexture->already_loaded++;
+					}
+				}
+			}
 		}
 	}
 }
